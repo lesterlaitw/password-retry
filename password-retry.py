766cb2222d8384e69ee0e01b91b2cad173f01200
Lester Lai <lester.lai.tw@gmail.com>
2019-04-23 09:58:15 +0800

    teacher version

--- password-retry.py
+++ password-retry.py
@@ -1,13 +1,31 @@
-x = 0
-while x < 3:
-	x = x + 1
-	password = input('請輸入密碼: ')
-	if password == 'a12345678':
+#看題做答自寫
+#x = 0
+#while x < 3:
+#	x = x + 1
+#	password = input('請輸入密碼: ')
+#	if password == 'a12345678':
+#		print('登入成功!')
+#		break		
+#	else:
+#		print('密碼錯誤!')
+#		if x < 3:
+#			print('還剩',3 - x ,'機會!')
+#		else:
+#			print('GG~')
+
+
+#講師版本
+password = 'a12345678'
+i = 3 #剩餘次數
+while i >0:
+	i = i - 1
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
 		print('登入成功!')
-		break		
+		break #逃出迴圈 
 	else:
 		print('密碼錯誤!')
-		if x < 3:
-			print('還剩',3 - x ,'機會!')
+		if i > 0:
+			print('還有', i,'次機會')
 		else:
-			print('GG~')
+			print('沒機會嘗試了!')
